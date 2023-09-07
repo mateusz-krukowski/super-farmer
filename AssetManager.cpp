7d1ed8b43eb3f4c516b47da64195bd43a33da7ee
mateusz-krukowski <matwoz2001@icloud.com>
2023-09-07 18:15:42 +0200

    gui fixed on linux with cmake

--- AssetManager.cpp
+++ AssetManager.cpp
@@ -2,8 +2,8 @@
 #include "AssetManager.h"
 
 AssetManager::AssetManager() {
-	 AtticAntique = LoadFontEx("assets/AtticAntique.ttf",50,0, NULL);
-	 Helvetica = LoadFontEx("assets/Helvetica.ttf", 50, 0, NULL);
+	 AtticAntique = LoadFontEx("assets/AtticAntique.ttf",50,0, 0);
+	 Helvetica = LoadFontEx("assets/Helvetica.ttf", 50, 0, 0);
 }
 AssetManager::~AssetManager() {
 	UnloadFont(AtticAntique);    // Default font unloading
